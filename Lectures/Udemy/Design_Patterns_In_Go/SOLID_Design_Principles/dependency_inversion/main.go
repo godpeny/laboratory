b1f53593f22bebe3ed28ab1bc8f4117e44d5b243
godpeny <slsnsepdpd@gmail.com>
2023-07-16 00:37:10 +0900

    feat: [GSMSEPLC-15] finish "Design Patterns In Go" lecture. ---  - finish "Design Patterns In Go" lecture.  - studying "dependency-inversion" principle.

---
+++ Lectures/Udemy/Design_Patterns_In_Go/SOLID_Design_Principles/dependency_inversion/main.go
@@ -0,0 +1,92 @@
+// https://en.wikipedia.org/wiki/Dependency_inversion_principle
+package main
+
+import "fmt"
+
+// Dependency Inversion Principle
+// High Level Module should not depend on Low Level Module
+// Both should depend on abstractions (abstraction means interface in Go typically)
+
+// LLM
+type Relationship int
+
+const (
+	Parent Relationship = iota
+	Child
+	Sibling
+)
+
+type Person struct {
+	name string
+	// other useful stuff here
+}
+
+type Info struct {
+	from         *Person
+	relationship Relationship
+	to           *Person
+}
+
+type Relationships struct {
+	relations []Info
+}
+
+func (rs *Relationships) FindAllChildrenOf(name string) []*Person {
+	result := make([]*Person, 0)
+
+	for i, v := range rs.relations {
+		if v.relationship == Parent &&
+			v.from.name == name {
+			result = append(result, rs.relations[i].to)
+		}
+	}
+
+	return result
+}
+
+func (rs *Relationships) AddParentAndChild(parent, child *Person) {
+	rs.relations = append(rs.relations,
+		Info{parent, Parent, child})
+	rs.relations = append(rs.relations,
+		Info{child, Child, parent})
+}
+
+// HLM
+// use interface instead of using low level module struct directly for abstraction
+type Research struct {
+	// relationships Relationships
+	browser RelationshipBrowser // low-level
+}
+
+type RelationshipBrowser interface {
+	FindAllChildrenOf(name string) []*Person
+}
+
+func (r *Research) Investigate() {
+	//relations := r.relationships.relations
+	//for _, rel := range relations {
+	//	if rel.from.name == "John" &&
+	//		rel.relationship == Parent {
+	//		fmt.Println("John has a child called", rel.to.name)
+	//	}
+	//}
+
+	// use abstraction to loosely coupled with low-level module
+	for _, p := range r.browser.FindAllChildrenOf("John") {
+		fmt.Println("John has a child called", p.name)
+	}
+}
+
+func main() {
+	parent := Person{"John"}
+	child1 := Person{"Chris"}
+	child2 := Person{"Matt"}
+
+	// low-level module
+	relationships := Relationships{}
+	relationships.AddParentAndChild(&parent, &child1)
+	relationships.AddParentAndChild(&parent, &child2)
+
+	research := Research{&relationships}
+	research.Investigate()
+}
