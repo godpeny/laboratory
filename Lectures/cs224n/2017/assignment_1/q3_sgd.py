7fc5f5a0fc1bbc63475caf05f485268f067b790b
yanay <slsnsepdpd@gmail.com>
2024-02-04 16:57:08 +0900

    study: [GSMSEPLC-24] Taking Lecture CS224n : remove answers on 'q3_sgd.py' ---  - remove answers on 'q3_sgd.py'.

--- Lectures/cs224n/2017/assignment_1/q3_sgd.py
+++ Lectures/cs224n/2017/assignment_1/q3_sgd.py
@@ -84,9 +84,6 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         cost = None
         ### YOUR CODE HERE
-        cost, grad = f(x)
-        x -= step * grad
-        postprocessing(x)
         ### END YOUR CODE
 
         if iter % PRINT_EVERY == 0:
