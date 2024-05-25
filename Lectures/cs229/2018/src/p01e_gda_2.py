a82e443787ef3696248bdf51457b228471dbb41f
godpeny <slsnsepdpd@gmail.com>
2024-05-25 11:39:58 +0900

    study: [GSMSEPLC-27] Taking Lecture CS229 : remove "p01e_gda_sol.py" and replace it with "p01e_gda_2.py" ---  - remove "p01e_gda_sol.py" and replace it with "p01e_gda_2.py".  - this will include different way of solving "p01e_gda".

---
+++ Lectures/cs229/2018/src/p01e_gda_2.py
@@ -0,0 +1,69 @@
+import numpy as np
+import util
+
+from linear_model import LinearModel
+
+"""
+referencing solution to make "p01e_gda" more profound.
+"""
+
+def main(train_path, eval_path, pred_path):
+    """Problem 1(e): Gaussian discriminant analysis (GDA)
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        eval_path: Path to CSV file containing dataset for evaluation.
+        pred_path: Path to save predictions.
+    """
+    # Load dataset
+    x_train, y_train = util.load_dataset(train_path, add_intercept=False)
+
+    # *** START CODE HERE ***
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=False)
+
+    clf = GDA()
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_eval)
+
+    for y_pred_i in y_pred:
+        if y_pred_i > 0.5:
+            print("True")
+        else:
+            print("False")
+
+    # *** END CODE HERE ***
+
+
+class GDA(LinearModel):
+    """Gaussian Discriminant Analysis.
+
+    Example usage:
+        > clf = GDA()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+
+    def fit(self, x, y):
+        """Fit a GDA model to training set given by x and y.
+
+        Args:
+            x: Training example inputs. Shape (m, n).
+            y: Training example labels. Shape (m,).
+
+        Returns:
+            theta: GDA model parameters.
+        """
+        # *** START CODE HERE ***
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (m, n).
+
+        Returns:
+            Outputs of shape (m,).
+        """
+        # *** START CODE HERE ***
+        # *** END CODE HERE
