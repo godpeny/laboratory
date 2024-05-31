1e2c5f5d8bacd805b736593fe255f6e1fb3999a3
godpeny <slsnsepdpd@gmail.com>
2024-05-31 23:43:36 +0900

    study: [GSMSEPLC-27] Taking Lecture CS229 : finish implementing "d" in "p02cde_posonly.py" ---  - finish implementing "d" in "p02cde_posonly.py".

--- Lectures/cs229/2018/src/p02cde_posonly.py
+++ Lectures/cs229/2018/src/p02cde_posonly.py
@@ -26,19 +26,24 @@ def main(train_path, valid_path, test_path, pred_path):
     pred_path_e = pred_path.replace(WILDCARD, 'e')
 
     # *** START CODE HERE ***
+    model = LogisticRegression()
+
     # Part (c): Train and test on true labels
     # Make sure to save outputs to pred_path_c
-    x_train, label_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
-    x_test, label_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
+    x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
+    x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
 
-    model = LogisticRegression()
-    label_hat = model.fit(x_train, label_train)
-
-    util.plot(x_test, label_test, model.theta, pred_path_c)
-
+    t_hat = model.fit(x_train, t_train)
+    util.plot(x_test, t_test, model.theta.squeeze(axis=0), '{}.png'.format(pred_path_c[:-4]))
 
     # Part (d): Train on y-labels and test on true labels
     # Make sure to save outputs to pred_path_d
+    _, y_train = util.load_dataset(train_path, label_col='y', add_intercept=True)
+    _, y_test = util.load_dataset(test_path, label_col='y', add_intercept=True)
+
+    y_hat = model.fit(x_train, y_train)
+    util.plot(x_test, y_test, model.theta.squeeze(axis=0), '{}.png'.format(pred_path_d[:-4]))
+
     # Part (e): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs to pred_path_e
     # *** END CODER HERE
