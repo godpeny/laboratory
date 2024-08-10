a8d86e77729c57a2e2a443ab09b9cc26a3753d60
godpeny <slsnsepdpd@gmail.com>
2024-08-10 19:29:56 +0900

    study: [GSMSEPLC-27] Taking Lecture CS229 : finish solving "p05c_tau.py" --- - finish solving "p05c_tau.py".

--- Lectures/cs229/2018/src/p05c_tau.py
+++ Lectures/cs229/2018/src/p05c_tau.py
@@ -1,3 +1,5 @@
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 import util
@@ -20,8 +22,35 @@ def main(tau_values, train_path, valid_path, test_path, pred_path):
 
     # *** START CODE HERE ***
     # Search tau_values for the best tau (lowest MSE on the validation set)
+    tau_values = [3e-2, 5e-2, 1e-1, 5e-1, 1e0, 1e1]
     # Fit a LWR model with the best tau value
     # Run on the test set to get the MSE value
+    x_eval, y_eval = util.load_dataset(valid_path, add_intercept=True)
+    x_test, y_test = util.load_dataset(test_path, add_intercept=True)
+
+    lowest_mse = math.inf
+    best_y_pred = np.zeros(x_eval.shape[0],)
+
+    for tau in tau_values:
+        lwr = LocallyWeightedLinearRegression(tau=tau)
+        lwr.fit(x_train, y_train)
+        y_pred = lwr.predict(x_eval)
+
+        mse = np.mean((y_eval - y_pred)**2)
+
+        if mse < lowest_mse:
+            lowest_mse = mse
+            best_y_pred = y_pred
+
     # Save predictions to pred_path
+    np.savetxt(pred_path, y_pred)
     # Plot data
+    plt.figure()
+    plt.plot(x_train, y_train, 'bx', linewidth=2)
+    plt.plot(x_eval, best_y_pred, 'ro', linewidth=2)
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    print(lowest_mse)
+    plt.show()
     # *** END CODE HERE ***
