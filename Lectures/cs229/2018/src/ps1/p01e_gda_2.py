e008c6967d8495feb3a77cf22f3bf801b1ad50bf
godpeny <slsnsepdpd@gmail.com>
2024-08-22 23:43:49 +0900

    study: [GSMSEPLC-27] Taking Lecture CS229 : Setting Up for PS#2 --- - Setting Up for PS#2 - add data_plot function for 'p01_lr.py' - question 2. - rearrange the directories for co-existing 'ps1' and 'ps2'

--- Lectures/cs229/2018/src/ps1/p01e_gda_2.py
+++ Lectures/cs229/2018/src/ps1/p01e_gda_2.py
@@ -1,4 +1,3 @@
-import numpy as np
 import util
 
 from linear_model import LinearModel
---
+++ Lectures/cs229/2018/src/ps2/src/p01_lr.py
@@ -0,0 +1,91 @@
+# Important note: you do not have to modify this file for your homework.
+
+import util
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def calc_grad(X, Y, theta):
+    """Compute the gradient of the loss with respect to theta."""
+    m, n = X.shape
+
+    margins = Y * X.dot(theta)
+    probs = 1. / (1 + np.exp(margins))
+    grad = -(1./m) * (X.T.dot(probs * Y))
+
+    return grad
+
+
+def logistic_regression(X, Y):
+    """Train a logistic regression model."""
+    m, n = X.shape
+    theta = np.zeros(n)
+    learning_rate = 10
+
+    i = 0
+    while True:
+        i += 1
+        prev_theta = theta
+        grad = calc_grad(X, Y, theta)
+        theta = theta - learning_rate * grad
+        if i % 10000 == 0:
+            print('Finished %d iterations' % i)
+            print(np.linalg.norm(prev_theta - theta))
+        if np.linalg.norm(prev_theta - theta) < 1e-15:
+            print('Converged in %d iterations' % i)
+            break
+    return
+
+def data_plot():
+    x, y = util.load_csv('../data/ds1_a.csv')
+
+    # Create a scatter plot
+    for i in range(len(y)):
+        if y[i] == 1:
+            plt.scatter(x[i][0], x[i][1], marker='o', color='blue')
+        elif y[i] == -1:
+            plt.scatter(x[i][0], x[i][1], marker='x', color='red')
+
+    # Add labels
+    plt.xlabel('x_0')
+    plt.ylabel('x_1')
+
+    # Show plot
+    plt.title('Scatter plot of x_0 vs x_1 with y labels')
+    plt.grid(True)
+    plt.show()
+
+    x, y = util.load_csv('../data/ds1_b.csv')
+
+    # Create a scatter plot
+    for i in range(len(y)):
+        if y[i] == 1:
+            plt.scatter(x[i][0], x[i][1], marker='o', color='blue')
+        elif y[i] == -1:
+            plt.scatter(x[i][0], x[i][1], marker='x', color='red')
+
+    # Add labels
+    plt.xlabel('x_0')
+    plt.ylabel('x_1')
+
+    # Show plot
+    plt.title('Scatter plot of x_0 vs x_1 with y labels')
+    plt.grid(True)
+    plt.show()
+
+def main():
+    print('==== Training model on data set A ====')
+    Xa, Ya = util.load_csv('../data/ds1_a.csv', add_intercept=True)
+    logistic_regression(Xa, Ya)
+
+    x_b, y_b = util.load_csv('../data/ds1_b.csv')
+    data_plot(x_b, y_b)
+
+    print('\n==== Training model on data set B ====')
+    Xb, Yb = util.load_csv('../data/ds1_b.csv', add_intercept=True)
+    logistic_regression(Xb, Yb)
+
+
+if __name__ == '__main__':
+    # main()
+    data_plot()
