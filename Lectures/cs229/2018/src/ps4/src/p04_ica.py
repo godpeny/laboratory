20ec92a9506d2a8d54b70f4b82e3dbded36cb874
yanay <slsnsepdpd@gmail.com>
2025-06-25 19:01:20 +0800

    study: [GSMSEPLC-27] Taking Lecture CS229 : Implementing 'p04_ica.py' by myself ---  - Implementing 'p04_ica.py' by myself.

--- Lectures/cs229/2018/src/ps4/src/p04_ica.py
+++ Lectures/cs229/2018/src/ps4/src/p04_ica.py
@@ -21,6 +21,7 @@ def update_W(W, x, learning_rate):
     """
     
     # *** START CODE HERE ***
+    updated_W = W + learning_rate * (np.linalg.inv(W.T) - np.sign(W @ x)@x.T)
     # *** END CODE HERE ***
 
     return updated_W
@@ -42,6 +43,7 @@ def unmix(X, W):
 
 
     # *** START CODE HERE ***
+    S = X@W
     # *** END CODE HERE ***
 
     return S
