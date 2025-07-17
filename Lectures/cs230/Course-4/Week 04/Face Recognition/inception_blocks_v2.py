e6792958d165762d1f43e957a25fb6ad8c27e3ee
yanay <slsnsepdpd@gmail.com>
2025-07-17 23:48:28 +0900

    study: [GSMSEPLC-35] Taking Lecture CS230 : Fix outdated libraries in assignment of C4M4 (Course-4_Weak-4): Face Recognition for the Happy House - v3 ---  - Fix outdated libraries in assignment of C4M4 (Course-4_Weak-4): Face Recognition for the Happy House - v3.

--- Lectures/cs230/Course-4/Week 04/Face Recognition/inception_blocks_v2.py
+++ Lectures/cs230/Course-4/Week 04/Face Recognition/inception_blocks_v2.py
@@ -5,10 +5,19 @@ from numpy import genfromtxt
 from keras import backend as K
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
-from keras.layers.normalization import BatchNormalization
-from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import (
+    BatchNormalization,
+    MaxPooling2D,
+    AveragePooling2D,
+    Concatenate,
+    Lambda,
+    Flatten,
+    Dense
+)
+
 import fr_utils
-from keras.layers.core import Lambda, Flatten, Dense
+
+
 
 def inception_block_1a(X):
     """
